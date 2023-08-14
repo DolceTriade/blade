aeb5d75aa7e475e3691797a7b2c9bf1f41523f39
Harsh Modi <hmodi@aviatrix.com>
2023-08-13 23:09:41 -0700

    Add basic helloworld with rust + rules_nixpkgs

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "BLADE";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: (flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter.default = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bazel_6
+          bazel-buildtools
+          bazel-watcher
+          cargo
+          rustc
+        ];
+        shellhook = ''
+          unalias ls
+        '';
+      };
+    }));
+}
