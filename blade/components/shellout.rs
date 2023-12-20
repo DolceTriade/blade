a28cce3b5f7622580e7cf9fa129e1d7d4bcb04d2
Harsh Modi <hmodi@aviatrix.com>
2023-12-19 16:51:12 -0800

    properly scroll on shellout update

--- blade/components/shellout.rs
+++ blade/components/shellout.rs
@@ -2,9 +2,8 @@ use ansi_to_html;
 use leptos::*;
 use leptos_dom::html::*;
 
-pub fn scroll_bottom(el: HtmlElement<AnyElement>) {
-    let any = el.into_any();
-    if let Some(c) = any.last_element_child() {
+pub fn scroll_bottom(el: HtmlElement<Div>) {
+    if let Some(c) = el.last_element_child() {
         c.scroll_into_view_with_bool(false)
     }
 }
@@ -15,10 +14,17 @@ pub fn ShellOut(
     #[prop(into)]
     text: MaybeSignal<String>
 ) -> impl IntoView {
+    let nr = create_node_ref::<Div>();
+    let t = text.clone();
+    create_effect(move|_| {
+        // Empty callback so this is called on update.
+        t.with(|_|{});
+        if let Some(nr) = nr.get() {
+            scroll_bottom(nr);
+        }
+    });
     view! {
-        <div
-            use:scroll_bottom
-            class="bg-gray-800 text-white p-4 rounded-lg overflow-auto overflow-x-auto"
+        <div _ref=nr class="bg-gray-800 text-white p-4 rounded-lg overflow-auto overflow-x-auto"
         >
             {move || match ansi_to_html::convert_escaped(&text.get()) {
                 Err(err) => view! { <div>{format!("mistake: {:#?}", err)}</div> },
