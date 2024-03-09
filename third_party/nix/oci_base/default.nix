9fd0bd97bc4501c24e079fd517fcbd9515cad4e1
Harsh Modi <hmodi@aviatrix.com>
2024-03-09 01:46:13 +0000

    add back missing oci_base dep

--- third_party/nix/oci_base/default.nix
+++ third_party/nix/oci_base/default.nix
@@ -4,6 +4,7 @@ let
     postgresql.lib
     sqlite.out
     openssl
+    libgcc.lib
   ];
   closure = with pkgs; builtins.toString (lib.strings.splitString "\n" (builtins.readFile "${closureInfo {rootPaths = packages;}}/store-paths"));
 in
