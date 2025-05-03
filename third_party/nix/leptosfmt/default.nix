1d0ad057a3ced316adbac72fa1a206fe14077fd7
DolceTriade <vcelestialragev@gmail.com>
2025-05-03 03:49:23 -0700

    update leptosfmt

--- third_party/nix/leptosfmt/default.nix
+++ third_party/nix/leptosfmt/default.nix
@@ -1,25 +1,31 @@
 {
   lib,
-  fetchFromGitHub,
   rustPlatform,
+  fetchFromGitHub,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "leptosfmt";
-  version = "7fec90b22e1dac9a4649aefbccee2bffc449fbb3";
+  version = "8b4194ba33eee417ababdd15498940014fd6d237";
 
   src = fetchFromGitHub {
     owner = "bram209";
-    repo = pname;
-    rev = version;
-    hash = "sha256-kozg49iWJbB5RZomVu6aLStv+YTjcsGD3sUO4tjS5r4=";
+    repo = "leptosfmt";
+    rev = "8b4194ba33eee417ababdd15498940014fd6d237";
+    hash = "sha256-F06Ag99rCn3qZywdxyP7ULOgyhbSzWNe+drBDZJWVxo=";
+    fetchSubmodules = true;
   };
 
-  cargoHash = "sha256-hpDj9FKYX9Ksq9lgsVjFHA1HtfJ/LyC3Q5esIUMIWMs=";
+  cargoHash = "sha256-Q2S34G4pcYv8sC/gE2+Q5ovVrrLBNV/Ltt0Ex/vuJTU=";
 
   meta = with lib; {
-    description = "A formatter for the leptos view! macro.";
+    description = "Formatter for the leptos view! macro";
+    mainProgram = "leptosfmt";
     homepage = "https://github.com/bram209/leptosfmt";
-    license = licenses.asl20;
-    maintainers = [];
+    changelog = "https://github.com/bram209/leptosfmt/blob/${src.rev}/CHANGELOG.md";
+    license = with licenses; [
+      asl20
+      mit
+    ];
+    maintainers = with maintainers; [figsoda];
   };
 }
