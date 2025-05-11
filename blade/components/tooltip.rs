ad7950e801749ac277a32d1c3308209e94ca757c
Harsh Modi <hmodi@aviatrix.com>
2025-05-11 16:30:18 -0700

    tooltip: Allow setting an x/y offset

--- blade/components/tooltip.rs
+++ blade/components/tooltip.rs
@@ -2,7 +2,12 @@ use leptos::{html, prelude::*};
 
 #[allow(non_snake_case)]
 #[component]
-pub fn Tooltip<F, IV>(children: Children, tooltip: F) -> impl IntoView
+pub fn Tooltip<F, IV>(
+    children: Children,
+    tooltip: F,
+    #[prop(optional)] offset_x: f64,
+    #[prop(optional)] offset_y: f64,
+) -> impl IntoView
 where
     F: Fn() -> IV,
     IV: IntoView,
@@ -13,9 +18,16 @@ where
             el.parent_element()
                 .map(|s| {
                     let body = document().body().unwrap().get_bounding_client_rect();
-                    s.get_bounding_client_rect().y() - body.y()
+                    (body, s.get_bounding_client_rect())
                 })
-                .map(|t| el.set_attribute("style", &format!("top: {t}px")).ok());
+                .map(|rects| {
+                    let top = rects.1.y() - rects.0.y() + offset_y;
+                    // arbitrary offset to make things line up better.
+                    let left = rects.1.x() - rects.0.x() + offset_x - 3.0;
+                    el.set_attribute("style", &format!("top: {top}px; left: {left}px;"))
+                        .ok()
+                })
+                .unwrap();
         }
     };
     view! {
