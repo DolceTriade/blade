7bde812db9e84fa2fdd8cf3453817a4c2bb0e3ef
DolceTriade <vcelestialragev@gmail.com>
2025-07-06 00:22:40 -0700

    wip

--- blade/components/testrunlist.rs
+++ blade/components/testrunlist.rs
@@ -1,9 +1,11 @@
+use anyhow::anyhow;
 use leptos::{either::Either, prelude::*};
 use leptos_router::{components::A, hooks::use_location};
 use web_sys::KeyboardEvent;
 
 use crate::components::{
     accordion::*,
+    card::Card,
     list::*,
     searchbar::Searchbar,
     statusicon::StatusIcon,
@@ -26,6 +28,44 @@ fn status_weight(s: state::Status) -> u8 {
         state::Status::Skip => 3,
         state::Status::Success => 4,
         _ => 5,
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum SortType {
+    Alphabetical,
+    Duration,
+    NoSort,
+}
+
+impl std::str::FromStr for SortType {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "alphabetical" => Ok(SortType::Alphabetical),
+            "duration" => Ok(SortType::Duration),
+            "nosort" => Ok(SortType::NoSort),
+            _ => Err(anyhow!("failed to parse {s} into SortType")),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum SortOrder {
+    Descending,
+    Ascending,
+}
+
+impl std::str::FromStr for SortOrder {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "descending" => Ok(SortOrder::Descending),
+            "ascending" => Ok(SortOrder::Ascending),
+            _ => Err(anyhow!("failed to parse {s} into SortOrder")),
+        }
     }
 }
 
@@ -64,22 +104,43 @@ fn junit_status_weight(s: &junit_parser::TestStatus) -> u8 {
     }
 }
 
-fn sort_tests(cases: &[TestListItem]) -> Vec<&TestListItem> {
-    let mut cases = cases.iter().collect::<Vec<_>>();
-    cases.sort_unstable_by(|a, b| {
+fn sort_test_list_items(
+    cases: &[TestListItem],
+    sort_by: SortType,
+    sort_order: SortOrder,
+) -> Vec<TestListItem> {
+    let mut vec = cases.to_vec();
+
+    vec.sort_unstable_by(|a, b| {
         let a_s = junit_status_weight(&a.2);
         let b_s = junit_status_weight(&b.2);
         if a_s != b_s {
             return a_s.cmp(&b_s);
         }
-        a.1.cmp(&b.1)
+
+        match sort_by {
+            SortType::Duration => a.3.partial_cmp(&b.3).unwrap(),
+            SortType::Alphabetical => a.1.partial_cmp(&b.1).unwrap(),
+            SortType::NoSort => std::cmp::Ordering::Equal,
+        }
     });
-    cases
+
+    if matches!(sort_order, SortOrder::Ascending) {
+        vec.reverse();
+    }
+    vec
 }
 
 #[allow(non_snake_case)]
 #[component]
-pub fn TestRunList() -> impl IntoView {
+pub fn TestRunList(
+    sort_by: ReadSignal<SortType>,
+    set_sort_by: WriteSignal<SortType>,
+    sort_order: ReadSignal<SortOrder>,
+    set_sort_order: WriteSignal<SortOrder>,
+    hide_success: ReadSignal<bool>,
+    set_hide_success: WriteSignal<bool>,
+) -> impl IntoView {
     let test = expect_context::<Memo<Result<state::Test, String>>>();
     let xml = expect_context::<LocalResource<Option<junit_parser::TestSuites>>>();
     let click = move |test: String| {
@@ -94,8 +155,57 @@ pub fn TestRunList() -> impl IntoView {
     };
 
     view! {
-        <div class="p-xs">
+        <div class="p-xs flex flex-row justify-between">
             <Searchbar id="search" placeholder="Filter tests..." keyup=search_key />
+            <div class="group p-1 place-self-center align-self-center flex flex-col">
+                <img class="h-4 w-4 dark:invert" src="/assets/sort.svg" />
+                <div class="hidden group-hover:flex group-hover:fixed flex-row">
+                    <Card class="p-2">
+                        <label>Sorting</label>
+                        <select
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            id="sort_type_dropdown"
+                            on:change=move |ev| {
+                                let val = event_target_value(&ev).parse::<SortType>().unwrap();
+                                if sort_by.read() == val {
+                                    return;
+                                }
+                                set_sort_by.set(val);
+                            }
+                        >
+                            <option value="NoSort">No Sort</option>
+                            <option value="Alphabetical">Alphabetical</option>
+                            <option value="Duration">Duration</option>
+                        </select>
+                        <select
+                            id="sort_order_dropdown"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            on:change=move |ev| {
+                                let val = event_target_value(&ev).parse::<SortOrder>().unwrap();
+                                if sort_order.read() == val {
+                                    return;
+                                }
+                                set_sort_order.set(val);
+                            }
+                        >
+                            <option value="Descending">Descending</option>
+                            <option value="Ascending">Ascending</option>
+                        </select>
+                        <span>
+                            <label>Hide Success</label>
+                            <input
+                                class="ml-4"
+                                type="checkbox"
+                                prop:checked=move || hide_success.get()
+                                on:change=move |ev| {
+                                    let val = event_target_checked(&ev);
+                                    set_hide_success(val);
+                                }
+                            />
+                        </span>
+                    </Card>
+                </div>
+            </div>
         </div>
         <Accordion>
             {move || {
@@ -133,7 +243,7 @@ pub fn TestRunList() -> impl IntoView {
                                             );
                                             let tooltip_clone = tooltip.clone();
                                             view! {
-                                                <ListItem hide=Signal::derive(|| false)>
+                                                <ListItem hide=Signal::derive(move || false)>
                                                     <A href=link>
                                                         <div class="flex items-center justify-start w-full">
                                                             <span class="float-left">
@@ -205,13 +315,16 @@ pub fn TestRunList() -> impl IntoView {
                                                             .collect::<Vec<_>>()
                                                     })
                                                     .map(|c| {
-                                                        sort_tests(&c).into_iter().cloned().collect::<Vec<_>>()
+                                                        sort_test_list_items(&c, sort_by.get(), sort_order.get())
+                                                            .into_iter()
+                                                            .collect::<Vec<_>>()
                                                     })
                                                     .unwrap_or_default()
                                             }
 
                                             key=move |c| (c.0.clone(), c.1.clone())
                                             children=move |c| {
+                                                let test_status_clone = c.2.clone();
                                                 let test_name = c.1.clone();
                                                 let tooltip = test_name.clone();
                                                 let filter_name = test_name.clone();
@@ -222,6 +335,11 @@ pub fn TestRunList() -> impl IntoView {
                                                     <ListItem hide=Signal::derive(move || {
                                                         !filter.get().is_empty()
                                                             && !filter_name.contains(&filter.get())
+                                                            || (hide_success.get()
+                                                                && matches!(
+                                                                    junit_status_to_status(test_status_clone.clone()),
+                                                                    state::Status::Success
+                                                                ))
                                                     })>
                                                         <div
                                                             on:click=move |_| {
